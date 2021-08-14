20e6bf1467d2f10df76efacc0ba35c184347d802
User <TELIDAS>
2021-08-14 21:21:47 +0600

    db migrations commit

--- dev_today/settings.py
+++ dev_today/settings.py
@@ -87,10 +87,15 @@ WSGI_APPLICATION = 'dev_today.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'news_board',
+            'USER': 'news_user',
+            'PASSWORD': 'password',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 }
 
 # Password validation
